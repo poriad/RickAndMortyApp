981de8e60cebfe29e83d9d0dcc3e61290454f7a5
poriad <alum.poriad@iesalixar.org>
2021-10-17 15:39:29 +0200

    17/10/2021 - SplashScreem icon now works in every phone size and navigation bottom now works

--- app/src/main/java/com/example/rickandmortyapp/ui/view/EpisodeActivity.kt
+++ app/src/main/java/com/example/rickandmortyapp/ui/view/EpisodeActivity.kt
@@ -1,6 +1,16 @@
 package com.example.rickandmortyapp.ui.view
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.rickandmortyapp.R
 
 class EpisodeActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        overridePendingTransition(R.anim.fadein, R.anim.fadeout)
+        super.onCreate(savedInstanceState)
+        supportActionBar?.setHomeAsUpIndicator(R.mipmap.rick)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
 }
